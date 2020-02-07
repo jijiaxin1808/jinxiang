7f9c0588de0978589b50a6135ce17e0f4741727d
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2020-02-07 21:43:24 +0800

    feat layout

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,22 +1,166 @@
-import React from 'react';
-import { connect } from 'dva';
-import styles from './IndexPage.css';
-import { Button } from  "antd";
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <Button>antd载入成功</Button>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import React, { useState } from "react";
+import { connect } from "dva";
+import menuConfig from "../utils/menuConfig";
+import { Link } from "react-router-dom";
+import "./index.less";
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider, Footer } = Layout;
+
+
+const MainMenu = ()=> {
+  const [collapsed, setcollapsed ] =  useState(false)
+
+    return (
+      <Layout>
+      <Sider
+        collapsible
+        collapsed = { collapsed }
+        onCollapse = {()=>{setcollapsed(!collapsed)}}
+        style={{
+          overflow: 'auto',
+          // height: '100vh',
+          // position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          {
+            menuConfig.map((item, index)=> {
+              if(item.children&&item.children.length) {
+                return (
+                  <SubMenu key={index} title={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
+                  {item.children.map((item, index) => (
+                    <Menu.Item key={item.index}><Link style = {{textDecoration:"none"}} to={item.url}>{item.title}</Link></Menu.Item>
+                  ))}
+                </SubMenu>
+                )
+              }
+              return (
+                <Menu.Item key={index}>
+                  <Link to = {item.url} style = {{textDecoration:"none"}}>
+                  <Icon type = {item.icon} />
+                  <span>{item.title}</span>
+                  </Link>
+                </Menu.Item>
+              )
+            })
+          }
+        </Menu>
+      </Sider>
+      <Layout >
+        <Header style={{ background: '#fff', padding: 0 }} >这里是header</Header>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+            ...
+            <br />
+            Really
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            long
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            ...
+            <br />
+            content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+    )
+  
 }
 
-IndexPage.propTypes = {
-};
 
-export default connect()(IndexPage);
+
+
+
+export default connect()(MainMenu);
---
+++ src/utils/menuConfig.js
@@ -0,0 +1,90 @@
+const menuConfig = [
+    {
+        title:"首页",
+        url: "/index",
+        icon:"home"
+    },
+    {
+        title:"功能",
+        icon:"tool",
+        children: [
+            {
+                title: "开屏页",
+                url:""
+            },
+            {
+                title: "热门搜索",
+                url:""
+            },
+            {
+                title: "轮播图",
+                url:""
+            },
+            {
+                title: "首页分类",
+                url:""
+            },
+            {
+                title: "全部分类",
+                url:""
+            },
+            {
+                title: "推送",
+                url:""
+            }
+        ]
+    },
+    {
+        title:"管理",
+        icon:"appstore",
+        children: [
+            {
+                title: "学校账户",
+                url:""
+            },
+            {
+                title: "题库",
+                url:""
+            },
+            {
+                title: "活动",
+                url:""
+            },
+            {
+                title: "用户",
+                url:""
+            }
+        ]
+    },
+    {
+        title:"美化",
+        icon:"heart",
+        url:"/beautify"
+    },
+    {
+        title:"数据可视",
+        icon:"table",
+        children: [
+            {
+                title: "用户分析",
+                url:""
+            },
+            {
+                title: "功能分析",
+                url:""
+            },
+            {
+                title: "订单分析",
+                url:""
+            },
+            {
+                title: "活动分析",
+                url:""
+            }
+        ]
+    },
+
+
+]
+
+export default menuConfig;
